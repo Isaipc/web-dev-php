7b5bab446830283ddf63292cadae68780d32b85f
Isai Polito <ispolitoc@gmail.com>
2020-05-14 00:59:16 -0500

    add db sql schema

---
+++ db.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS `web_dev_php`;
+
+CREATE DATABASE IF NOT EXISTS `web_dev_php`;
+
+USE `web_dev_php`;
+
+CREATE TABLE IF NOT EXISTS `account`(
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(60) NOT NULL,
+    `lastname` VARCHAR(120) NOT NULL,
+    `dni` VARCHAR(30) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(12) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `zipcode` MEDIUMINT NOT NULL,
+    `age` TINYINT NOT NULL,
+    `sex` ENUM('Hombre', 'Mujer') NOT NULL,
+    `address` VARCHAR(90) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP NOT NULL
+)
