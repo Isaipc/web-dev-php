1cb511dae0078ca27f2096aea9a6c1b52eca3dcf
Isai Polito <ispolitoc@gmail.com>
2020-05-16 03:49:21 -0500

    add new db.sql

---
+++ export.db.sql
@@ -0,0 +1,43 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.7.26 - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Volcando estructura de base de datos para web_dev_php
+CREATE DATABASE IF NOT EXISTS `web_dev_php` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `web_dev_php`;
+
+-- Volcando estructura para tabla web_dev_php.account
+CREATE TABLE IF NOT EXISTS `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `lastname` varchar(120) NOT NULL,
+  `dni` varchar(30) NOT NULL,
+  `password` varchar(191) NOT NULL,
+  `phone` varchar(12) NOT NULL,
+  `email` varchar(191) NOT NULL,
+  `zipcode` mediumint(9) NOT NULL,
+  `age` tinyint(4) NOT NULL,
+  `gender` enum('Hombre','Mujer') NOT NULL,
+  `address` varchar(90) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+-- Volcando datos para la tabla web_dev_php.account: 3 rows
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
