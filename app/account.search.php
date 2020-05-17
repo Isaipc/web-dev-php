611be04c1346d8ccda6f61208684a1ed416801e5
Isai Polito <ispolitoc@gmail.com>
2020-05-16 19:25:43 -0500

    improve account search

--- app/account.search.php
+++ app/account.search.php
@@ -5,11 +5,14 @@ include 'database/connection.php';
 if(isset($_GET['search'])){
     
     try{
-        $search = 'SELECT * FROM account WHERE name LIKE :name';
-        $sql = $conn->prepare($search);
+
+        $search = $_GET['search'] == '' ? 0 : 1;
+
+        $sql = $conn->prepare("SELECT * FROM account WHERE name LIKE :name OR :search = 0");
 
         $name = "%". $_GET['search'] . "%";
         $sql->bindParam(':name', $name, PDO::PARAM_STR);
+        $sql->bindParam(':search', $search);
         $sql->execute();
         
         $result = $sql->fetchAll();
